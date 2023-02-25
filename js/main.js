1a9e5abb63a9fd5aa8f468e2cdeaf52c986f68d3
William Harper <williamharper@Williams-MacBook-Air.local>
2023-02-25 16:19:07 -0500

    added images and meso cost

--- js/main.js
+++ js/main.js
@@ -12,91 +12,52 @@ boomChances.push(
     0.0,0.0,0.0,0.0,0.0,
     0.0,0.0,0.0,0.0,0.0,
     0.0,0.0,3.0,3.0,3.0,
-    10.0,10.0);
+    10.0,10.0
+);
 
-const safeguard = 1.00;
+const itemImgs = [];
+itemImgs["zerk"] = "img/Eqp_Berserked.webp";
+itemImgs["mep"] = "img/Eqp_Magic_Eyepatch.webp";
+itemImgs["belt"] = "img/Eqp_Dreamy_Belt.webp";
+itemImgs["et"] = "img/Eqp_Endless_Terror.webp";
+itemImgs["cfe"] = "img/Eqp_Commanding_Force_Earring.webp";
+itemImgs["sos"] = "img/Eqp_Source_of_Suffering.webp";
+itemImgs["sos20"] = "img/Eqp_Source_of_Suffering.webp";
+itemImgs["sos21"] = "img/Eqp_Source_of_Suffering.webp";
+itemImgs["sosrep"] = "img/Eqp_Source_of_Suffering.webp";
 
-function select()
+const itemLevel = [];
+itemLevel["zerk"] = 160.0;
+itemLevel["mep"] = 160.0;
+itemLevel["belt"] = 200.0;
+itemLevel["et"] = 200.0;
+itemLevel["cfe"] = 200.0;
+itemLevel["sos"] = 160.0;
+itemLevel["sos20"] = 160.0;
+itemLevel["sos21"] = 160.0;
+itemLevel["sosrep"] = 160.0;
+
+
+const safeguard = 1.05;
+
+function fullSend(currentStar, failCount, repeat, level)
 {
-    var e = document.getElementById("item");
-    var item = e.value;
-    
-    let pass = 0.0;
-    let boom = 0.0;
-
-    for (let i = 0; i < 10000; i++) { 
-
-        let currentStar = 0;
-        let failCount = 0;
-
-        if(item == "sos20")
-            currentStar = 20;
-        else if (item == "sos21")
-            currentStar = 21;
-        else if (item == "sosrep")
-            currentStar = 21;
-
-        while (true)
-        {
-            if (failCount >= 2)
-            {
-                currentStar += 1;
-                failCount = 0;
-            }
-            
-            let success = Math.random() * 100.0;
-            let boom = Math.random() * 100.0;
-
-            if (success < (successChances[currentStar] * safeguard))
-            {
-                currentStar += 1;
-                failCount = 0;
-            }
-            else if (boom < boomChances[currentStar])
-            {
-                boom += 1.0;
-                break;
-            }
-            else
-            {
-                if (currentStar > 10 && currentStar != 15 && currentStar != 20 && item != "sosrep")
-                {
-                    failCount += 1;
-                    currentStar -= 1;
-                }
-            }
-
-            if (currentStar >= 22)
-            {
-                pass += 1.0;
-                break;
-            }
-        }
-    }
-    document.getElementById("chance").innerHTML = Math.round(pass / 10000.0 * 100) + "%";
-
-}
-
-function tap()
-{
-    var e = document.getElementById("item");
-    var item = e.value;
-    
-    let pass = 0.0;
-    let boom = 0.0;
-
-    let currentStar = 0;
-    let failCount = 0;
-
-    if(item == "sos20")
-        currentStar = 20;
-    else if (item == "sos21")
-        currentStar = 21;
-    else if (item == "sosrep")
-        currentStar = 21;
-
+    let cost = 0.0;
     while (true)
     {
+        let deltaCost = 0.0;
+        if (currentStar < 10)
+            deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * ((currentStar + 1) )/ 2500.0 + 10.0);
+        else if (currentStar < 15)
+            deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * (Math.pow((currentStar + 1), 2.7))/ 40000.0 + 10.0);
+        else    
+            deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * (Math.pow((currentStar + 1), 2.7))/ 20000.0 + 10.0);
+
+        if (currentStar < 17 && currentStar > 11)
+            deltaCost = deltaCost * 2.0;
+        
+        cost += deltaCost;
+        
         if (failCount >= 2)
         {
             currentStar += 1;
@@ -113,13 +74,11 @@ function tap()
         }
         else if (boom < boomChances[currentStar])
         {
-            document.getElementById("result").innerHTML = "BOOMED! at " + currentStar;
-            boom += 1.0;
-            break;
+            return [currentStar, cost];
         }
         else
         {
-            if (currentStar > 10 && currentStar != 15 && currentStar != 20 && item != "sosrep")
+            if (currentStar > 10 && currentStar != 15 && currentStar != 20 && repeat == false)
             {
                 failCount += 1;
                 currentStar -= 1;
@@ -128,12 +87,80 @@ function tap()
 
         if (currentStar >= 22)
         {
-            document.getElementById("result").innerHTML = "22!";
-            pass += 1.0;
-            break;
+            return [currentStar, cost];
         }
     }
-    
+}
 
+function tapOnce(currentStar, failCount, repeat)
+{
+    
+}
+
+function select()
+{
+    var e = document.getElementById("item");
+    var item = e.value;
+
+    document.getElementById("itemImg").src = itemImgs[item];
+    
+    let pass = 0.0;
+    let boom = 0.0;
+
+    for (let i = 0; i < 10000; i++) { 
+
+        let currentStar = 0;
+        let failCount = 0;
+        let repeat = false;
+
+        if(item == "sos20")
+            currentStar = 20;
+        else if (item == "sos21")
+            currentStar = 21;
+        else if (item == "sosrep")
+        {
+            currentStar = 21;
+            repeat = true;
+        }
+
+        let result = fullSend(currentStar, failCount, repeat, itemLevel[item]);
+        if (result[0] != 22)
+            boom += 1.0;
+        else    
+            pass += 1.0;
+    }
+    document.getElementById("chance").innerHTML = Math.round(pass / 10000.0 * 100) + "%";
+
+}
+
+function tap()
+{
+    var e = document.getElementById("item");
+    var item = e.value;
+    
+    let pass = 0.0;
+    let boom = 0.0;
+
+    let currentStar = 0;
+    let failCount = 0;
+    let repeat = false;
+
+    if(item == "sos20")
+        currentStar = 20;
+    else if (item == "sos21")
+        currentStar = 21;
+    else if (item == "sosrep")
+    {
+        currentStar = 21;
+        repeat = true;
+    }
+
+    let result = fullSend(currentStar, failCount, repeat, itemLevel[item]);
+    if (result[0] != 22)
+        document.getElementById("result").innerHTML = "boomed at " + result[0];
+    else    
+        document.getElementById("result").innerHTML = result[0];
+    
+    document.getElementById("cost").innerHTML = Math.round(result[1]).toLocaleString("en-US");
 }
 
