2f6ddc9411e7e569d87e3f52123ea9ab328470e9
William Harper <williamharper@Williams-MacBook-Air.local>
2023-02-25 17:11:06 -0500

    added single tap button

--- js/main.js
+++ js/main.js
@@ -45,60 +45,72 @@ function fullSend(currentStar, failCount, repeat, level)
     let cost = 0.0;
     while (true)
     {
-        let deltaCost = 0.0;
-        if (currentStar < 10)
-            deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * ((currentStar + 1) )/ 2500.0 + 10.0);
-        else if (currentStar < 15)
-            deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * (Math.pow((currentStar + 1), 2.7))/ 40000.0 + 10.0);
-        else    
-            deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * (Math.pow((currentStar + 1), 2.7))/ 20000.0 + 10.0);
+        result = tapOnce(currentStar, failCount, repeat, level, cost);
+        currentStar = result[0];
+        cost = result[1];
+        boomed = result[2];
 
-        if (currentStar < 17 && currentStar > 11)
-            deltaCost = deltaCost * 2.0;
-        
-        cost += deltaCost;
-        
-        if (failCount >= 2)
-        {
-            currentStar += 1;
-            failCount = 0;
-        }
-        
-        let success = Math.random() * 100.0;
-        let boom = Math.random() * 100.0;
-
-        if (success < (successChances[currentStar] * safeguard))
-        {
-            currentStar += 1;
-            failCount = 0;
-        }
-        else if (boom < boomChances[currentStar])
-        {
-            return [currentStar, cost];
-        }
-        else
-        {
-            if (currentStar > 10 && currentStar != 15 && currentStar != 20 && repeat == false)
-            {
-                failCount += 1;
-                currentStar -= 1;
-            }
-        }
-
-        if (currentStar >= 22)
-        {
-            return [currentStar, cost];
-        }
+        if (currentStar == 22)
+            return [result[0], result[1]];
+        if (boomed == true)
+            return [result[0], result[1]];
     }
 }
 
-function tapOnce(currentStar, failCount, repeat)
+function tapOnce(currentStar, failCount, repeat, level, cost)
 {
+    let deltaCost = 0.0;
+    if (currentStar < 10)
+        deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * ((currentStar + 1) )/ 2500.0 + 10.0);
+    else if (currentStar < 15)
+        deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * (Math.pow((currentStar + 1), 2.7))/ 40000.0 + 10.0);
+    else    
+        deltaCost += 100.0 * (Math.round(Math.pow(level, 3.0)) * (Math.pow((currentStar + 1), 2.7))/ 20000.0 + 10.0);
+
+    if (currentStar < 17 && currentStar > 11)
+        deltaCost = deltaCost * 2.0;
     
+    cost += deltaCost;
+    
+    if (failCount >= 2)
+    {
+        currentStar += 1;
+        failCount = 0;
+    }
+    
+    let success = Math.random() * 100.0;
+    let boom = Math.random() * 100.0;
+
+    if (success < (successChances[currentStar] * safeguard))
+    {
+        currentStar += 1;
+        failCount = 0;
+    }
+    else if (boom < boomChances[currentStar])
+    {
+        return [currentStar, cost, true];
+    }
+    else
+    {
+        if (currentStar > 10 && currentStar != 15 && currentStar != 20 && repeat == false)
+        {
+            failCount += 1;
+            currentStar -= 1;
+        }
+    }
+
+    return [currentStar, cost, false];
 }
+
+let first = true;
 
 function select()
 {
+    first = true;
+
+    document.getElementById("result").innerHTML = '\xa0';
+    document.getElementById("cost").innerHTML = '\xa0';
+    
     var e = document.getElementById("item");
     var item = e.value;
 
@@ -133,13 +145,10 @@ function select()
 
 }
 
-function tap()
+function insta()
 {
     var e = document.getElementById("item");
     var item = e.value;
-    
-    let pass = 0.0;
-    let boom = 0.0;
 
     let currentStar = 0;
     let failCount = 0;
@@ -162,5 +171,66 @@ function tap()
         document.getElementById("result").innerHTML = result[0];
     
     document.getElementById("cost").innerHTML = Math.round(result[1]).toLocaleString("en-US");
+}
+
+let globalStar = 0;
+let globalCost = 0;
+let globalFail = 0;
+let globalRepeat = false;
+let globalItem = null;
+
+function tap()
+{
+    if (first == true)
+    {
+        globalStar = 0;
+        globalCost = 0;
+        globalFail = 0;
+        globalRepeat = false;
+
+        var e = document.getElementById("item");
+        globalItem = e.value;
+        
+        if(globalItem == "sos20")
+            globalStar = 20;
+        else if (globalItem == "sos21")
+            globalStar = 21;
+        else if (globalItem == "sosrep")
+        {
+            globalStar = 21;
+            globalRepeat = true;
+        }
+        
+        first = false;
+
+        document.getElementById("result").innerHTML = '\xa0';
+        document.getElementById("cost").innerHTML = '\xa0';
+
+        return;
+    }
+
+    result = tapOnce(globalStar, globalFail, globalRepeat, itemLevel[globalItem], globalCost);
+    globalStar = result[0];
+    globalCost = result[1];
+    boomed = result[2];
+
+    if (globalStar == 22)
+    {
+        document.getElementById("result").innerHTML = "22";
+        document.getElementById("cost").innerHTML = Math.round(globalCost).toLocaleString("en-US");
+        first = true;
+    }
+    else if (boomed == true)
+    {
+        document.getElementById("result").innerHTML = "BOOMED at " + globalStar;
+        document.getElementById("cost").innerHTML = Math.round(globalCost).toLocaleString("en-US");
+        first = true;
+    }
+    else
+    {
+        document.getElementById("result").innerHTML = globalStar;
+        document.getElementById("cost").innerHTML = Math.round(globalCost).toLocaleString("en-US");
+    }
+    
 }
 
